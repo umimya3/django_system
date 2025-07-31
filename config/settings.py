f06286ea788327eed70e6bd85c97bcacb58c1970
umimya3 <umimi@tuta.io>
2025-07-31 14:56:54 +0900

    Final attempt to trigger migrations

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,15 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+#nyahello world
+#lady go??
+
+
+#
+#
+#
+#
+#
 
 import os
 import dj_database_url
--- schedule/apps.py
+++ schedule/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
-
+import os
 
 class ScheduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schedule'
+
+    def ready(self):
+        # DEBUGモードの時だけ実行
+        # if os.environ.get('DEBUG') == 'True':
+        #     from django.contrib.auth import get_user_model
+        #     User = get_user_model()
+        #     username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        #     email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        #     password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword') # ★非常に危険なパスワード！必ず変更すること！
+
+        #     if not User.objects.filter(username=username).exists():
+        #         print(f"Creating superuser: {username}")
+        #         User.objects.create_superuser(username, email, password)
+        #         print("Superuser created successfully.")
+        #     else:
+        #         print(f"Superuser {username} already exists.")
