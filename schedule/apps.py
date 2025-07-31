b26fb5cbff57c2d39cd6965db7b99c0a6303e6f4
umimya <76414484+umimya3@users.noreply.github.com>
2025-07-31 17:14:52 +0900

    Add files via upload

--- schedule/apps.py
+++ schedule/apps.py
@@ -6,17 +6,17 @@ class ScheduleConfig(AppConfig):
     name = 'schedule'
 
     def ready(self):
-        # DEBUGモードの時だけ実行
-        # if os.environ.get('DEBUG') == 'True':
-        #     from django.contrib.auth import get_user_model
-        #     User = get_user_model()
-        #     username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-        #     email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-        #     password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword') # ★非常に危険なパスワード！必ず変更すること！
+         DEBUGモードの時だけ実行
+         if os.environ.get('DEBUG') == 'True':
+             from django.contrib.auth import get_user_model
+             User = get_user_model()
+             username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+             email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+             password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'adminpassword') # ★非常に危険なパスワード！必ず変更すること！
 
-        #     if not User.objects.filter(username=username).exists():
-        #         print(f"Creating superuser: {username}")
-        #         User.objects.create_superuser(username, email, password)
-        #         print("Superuser created successfully.")
-        #     else:
-        #         print(f"Superuser {username} already exists.")
+             if not User.objects.filter(username=username).exists():
+                 print(f"Creating superuser: {username}")
+                 User.objects.create_superuser(username, email, password)
+                 print("Superuser created successfully.")
+             else:
+                 print(f"Superuser {username} already exists.")
